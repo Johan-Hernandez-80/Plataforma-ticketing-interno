18a69192fc7b6342d84fbe836b50c190eee40525
Juan Molano <molano.juan.2629@eam.edu.co>
2025-09-04 15:18:14 -0500

    Añade repositories de las entidades con métodos default

---
+++ Backend/src/main/java/com/jsj/api/repository/ComentarioRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jsj.api.repository;
+
+import com.jsj.api.entity.Comentario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Juan José Molano Franco
+ */
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+
+}
